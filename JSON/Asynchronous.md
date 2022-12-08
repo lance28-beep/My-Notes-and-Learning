53e3268addd56b039c402019668f6b1dd24d72b4
Lance Valle <vallelance8@gmail.com>
2022-12-08 22:10:35 +0800

    promise

---
+++ JSON/Asynchronous.md
@@ -0,0 +1,28 @@
+## Asynchronous Code
+
+Lesson Overview
+
+- Explain what a callback is
+- Explain what a promise is
+- Explain the circumstances under which promises are better than callbacks
+- Explain what the .then() function does
+
+Callbacks
+
+- A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action. MDN.
+- Callbacks are functions that are executed asynchronously, or at a later time.
+- Instead of the code reading top to bottom procedurally, async programs may execute different functions at different times based on the order and the speed that earlier functions like http requests or file system reads happen.
+
+## Promises
+
+- Convert Callbacks to promisses
+- how to create Promisses
+- how to use Promisses
+- Why
+- Different phases of Promises
+- Error handling
+- Chain multiple Promises
+
+reminder:
+Synchronous - code blocks the execution of any line that comes after
+Asynchronous - code does not if i have two functions here that take five seconds to complete one synchronous and
--- JSON/script.js
+++ JSON/script.js
@@ -1,17 +1,14 @@
 // async function populate(){
 //     const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 
-
 // const request = new Request(requestURL);
 // const response = await fetch(request);
 // const superHeroes = await response.json();
-
 
 // populateHeader(superHeroes);
 // populateHeroes(superHeroes);
 // console.log(superHeroes)
 // }
-
 
 // function populateHeader(obj){
 //     const header = document.querySelector('header')
@@ -29,7 +26,7 @@
 // function populateHeroes(obj) {
 //     const section = document.querySelector('section');
 //     const heroes = obj.members;
-  
+
 //     for (const hero of heroes) {
 //       const myArticle = document.createElement('article');
 //       const myH2 = document.createElement('h2');
@@ -37,25 +34,25 @@
 //       const myPara2 = document.createElement('p');
 //       const myPara3 = document.createElement('p');
 //       const myList = document.createElement('ul');
-  
+
 //       myH2.textContent = hero.name;
 //       myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
 //       myPara2.textContent = `Age: ${hero.age}`;
 //       myPara3.textContent = 'Superpowers:';
-  
+
 //       const superPowers = hero.powers;
 //       for (const power of superPowers) {
 //         const listItem = document.createElement('li');
 //         listItem.textContent = power;
 //         myList.appendChild(listItem);
 //       }
-  
+
 //       myArticle.appendChild(myH2);
 //       myArticle.appendChild(myPara1);
 //       myArticle.appendChild(myPara2);
 //       myArticle.appendChild(myPara3);
 //       myArticle.appendChild(myList);
-  
+
 //       section.appendChild(myArticle);
 //     }
 //   }
@@ -111,7 +108,7 @@
 
 // for (let index = 0; index < array.length; index++) {
 //   const element = array[index];
-  
+
 // }
 
 // let pizza
@@ -127,18 +124,119 @@
 // orderPizza()
 // console.log( `eat ${pizza}`)
 
-function orderPizza(callback){
-  setTimeout(() => {
-    const pizza = "pizza"
-    callback(pizza)
-  },2000)
+// function orderPizza(callback){
+//   setTimeout(() => {
+//     const pizza = "pizza"
+//     callback(pizza)
+//   },2000)
+// }
+
+// function pizzaReady(pizza){
+//   console.log(`Eat the ${pizza}`)
+// }
+
+// orderPizza(pizzaReady)
+
+// console.log(`call Qoli`)
+
+// let p = new Promise((resolve, reject) => {
+//   let a = 1 + 2;
+//   if (a == 2) {
+//     resolve("success");
+//   } else {
+//     reject("failed");
+//   }
+// });
+
+// p.then((message) => {
+//   console.log(`this is in the then ${message}`)
+// }).catch((message) => {
+//   console.log(`This is in the cath ${message}`)
+// })
+
+// const userLeft = falsec
+
+// Get the toggle button and nav bar container elements
+// const toggleButton = document.querySelector('#toggle-nav');
+// const nav = document.querySelector('nav');
+
+// // Add an event listener to the toggle button that toggles the "show-nav" class on the nav bar container
+// toggleButton.addEventListener('click', () => {
+//   nav.classList.toggle('show-nav');
+// });
+
+// function getWeather() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("resolve");
+//     }, 8000);
+//     reject(new Error("reject"));
+//   });
+// }
+
+// getWeather()
+//   .then((resolve) => {
+//     console.log(`this resolve ${resolve}`);
+//   })
+//   .catch((err) => {
+//     console.error("this is error " + err);
+//   });
+
+// async function getData() {
+//   try {
+//     //console.loading
+//     console.log("loading.....");
+
+//     const responsePromise = new Promise((res, rej) => {
+//       fetch("https://jsonplaceholder.typicode.com/todos/1")
+//         .then((response) => {
+//           res(response);
+//         })
+//         .catch((err) => {
+//           rej(err);
+//         });
+//     });
+
+//     const response = await responsePromise;
+
+//     const data = await response.json();
+
+//     return data;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// const data = getData();
+
+// console.log(data);
+
+function fun1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("404");
+    }, 10000);
+  });
 }
 
-function pizzaReady(pizza){
-  console.log(`Eat the ${pizza}`)
+function fun2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("hello");
+    }, 5000);
+  });
 }
 
-orderPizza(pizzaReady)
+function onSuccess(data) {
+  console.log("success: " + data);
+}
 
+function onError(errorCode) {
+  console.log("error : " + errorCode);
+}
 
-console.log(`call Qoli`)
+function inTheEnd() {
+  console.log("finaly we be done yo");
+}
+
+fun1().then(fun2).then(onSuccess).catch(onError).finally(inTheEnd);
